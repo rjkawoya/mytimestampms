fc5bd675bcafa92326753f4ec928f7d67d575e94
R.J. Kawoya <89083293+rjkawoya@users.noreply.github.com>
2021-10-21 21:26:04 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,73 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+let resObject = {}
+
+app.get('/api/:input', (req, res) => {
+  let input = req.params.input
+  
+if(input.includes('-')){
+  /* Date String */
+  resObject['unix'] = new Date(input).getTime()
+  resObject['utc'] = new Date(input).toUTCString()
+}else{
+  /* Timestamp */
+  input = parseInt(input)
+
+  resObject['unix'] = new Date(input).getTime()
+  resObject['utc'] = new Date(input).toUTCString()
+}
+
+if(!resObject['utc'] || !resObject['unix']){
+  res.json({error: 'Invalid Date'})
+}
+
+  res.json(resObject)
+})
+
+app.get('/api', (req, res) => {
+  resObject['unix'] = new Date().getTime()
+  resObject['utc'] = new Date().toUTCString()
+   res.json(resObject)
+})
+
+
+
+
+
+
+ 
+
+
+
+
